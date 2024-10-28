592aa068d40654cf865e0760febb015d21c1e753
jackberrypassionfruit <pashayanjack@gmail.com>
2024-10-28 18:00:48 -0400

    my first commit!

---
+++ curious_data/build.sql
@@ -0,0 +1,21 @@
+create schema if not exists import.import;
+
+drop table if exists import.master_plan;
+
+create table import.master_plan(
+    start_time_utc text,
+    duration text,
+    date text,
+    team text,
+    spass_type text,
+    target text,
+    request_name text,
+    library_definition text,
+    title text,
+    description text
+);
+
+COPY import.master_plan
+FROM '/data/curious_data/data/master_plan.csv'
+WITH DELIMITER ',' HEADER CSV;
+
